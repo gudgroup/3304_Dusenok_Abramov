3de9541244dc8f73bba00bdd7fa2c8b0cb860c80
jongeit <gudasttv@gmail.com>
2015-06-25 00:59:24 +0300

    main class

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by jongeit on 25.06.15.
+ */
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+        Main frame = new Main();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 320);
+        frame.setVisible(true);
+
+    }
+
+}
